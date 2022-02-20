196713b932112abb5f3c8a711293ead78d1acb8d
NolannB14 <nolann.bougrainville@epitech.eu>
2022-02-20 12:49:35 +0100

    [MODIFY] students cell format

--- epi-career-meeting/src/components/Offer/OfferCase.jsx
+++ epi-career-meeting/src/components/Offer/OfferCase.jsx
@@ -1,15 +1,22 @@
 import React from 'react';
 
 function OfferCase(props) {
+  const formatStudents = (studentsList) => {
+    let formatStringStudents = "";
+
+    formatStringStudents += studentsList[0];
+    for (let i = 1; i < studentsList.length; i++) {
+      formatStringStudents += ', ';
+      formatStringStudents += studentsList[i];
+    }
+    return formatStringStudents;
+  };
+
   return (
     <tr>
       <th className="border border-slate-600">{props.index}</th>
       <th className="border border-slate-600">{props.name}</th>
-      <th className="border border-slate-600">
-        {props.students.map((element) =>
-          <span className="" key={element}>{element + '\t'}</span>
-        )}
-      </th>
+      <th className="border border-slate-600">{formatStudents(props.students)}</th>
     </tr>
   )
 }
